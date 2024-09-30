5302d14bd653ad4f0d0df5741c5cfd5acc0be429
Kenny <keny.guerra@ucsm.edu.pe>
2024-09-30 09:52:58 -0500

    Ejercicio1-Experiencia1

---
+++ PracticaNo6/Ejercicio1/experiencia1/App.js
@@ -0,0 +1,12 @@
+import React from 'react'; // Importa la librería React para poder usar JSX
+
+// Definición del componente 'App' como una función
+function App() {
+  return (
+    <div class="App">
+      <h1>¡Hola desde React con npm y Node.js!</h1>
+    </div>
+  );
+}
+
+export default App; // Exporta el componente
